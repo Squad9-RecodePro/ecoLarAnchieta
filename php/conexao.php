252308b9a20f354d08ddd62793f64a75d3258b46
FredericoReid <fredericoreid@hotmail.com>
2020-10-31 20:04:46 -0300

    Arquivo de conexao com o banco de dados

---
+++ php/conexao.php
@@ -0,0 +1,21 @@
+<?php
+
+//Aqui definimos os parametros para a conexao com o banco de dados
+define('Hostname', 'sql311.epizy.com');
+define('Username', 'epiz_27075315');
+define('Password', 'SByyuy8eL3Rwqrr');
+define('DB', 'epiz_27075315_XXX');
+
+//Fazemos uma conexao com o banco de dados passando os parametros definidos anteriormente
+$conexao = mysqli::__construct(Hostname, Username, Password, DB);
+
+//Verificamos se a conexão foi realizada com sucesso
+if(!$conexao){
+    echo "Não conectado";
+}
+
+else{
+    echo "Conectado com sucesso";
+}
+
+?>
