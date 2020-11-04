91b150b59921aea51295ab6460fd44d957a879b6
unknown <thamirez.bastoss@gmail.com>
2020-11-04 11:19:11 -0300

    inclusão do perfil

--- meuperfil.php
+++ meuperfil.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['nome'])) {
+    header('Location: index.php');
+    exit;
+}else{
+
+    $conn = mysqli_connect("localhost", "root", "", "sistema");
+
+    $postagens = $conn->query("SELECT * FROM postagens JOIN usuarios WHERE fk_usuario = id");
+
+    foreach ($postagens as $postagem){
+
+        echo $postagem 
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -7,7 +26,7 @@
     <title>EcoCasa - Cadastre-se</title>
     <link rel="icon" href="./images/iconlogo.svg" /> <!-- Icone no Title -->
 
-    <link rel="stylesheet" href="./css/cadastro.css" />
+    <link rel="stylesheet" href="#" />
 
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
@@ -31,13 +50,10 @@
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link text-dark" href="./login.html">Login</a>
-                    </li>
-                    <li class="nav-item">
-						<a class="nav-link text-dark" href="./cadastro.html">Cadastre-se</a>
+						<a class="nav-link text-dark" href="#">Meu Perfil</a>
 					</li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark" href="#">Meu Perfil</a>
+                        <a class="nav-link text-dark" href="sair.php">Sair</a>
                     </li>
                 </ul>
             </div>
