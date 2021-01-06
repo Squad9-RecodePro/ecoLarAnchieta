c7aaa6d0e5565bc5ff6f9cd5c5d1fa503750f7eb
FredericoReid <fredericoreid@hotmail.com>
2021-01-05 21:17:01 -0300

    Atualizado a estética do código para melhor entendimento

--- backend/login_usuario.php
+++ backend/login_usuario.php
@@ -6,16 +6,22 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 
 if (strlen($email) > 3 && strlen($senha) > 3) {
+    
     //criptografia da senha
     $senha_cripto = md5($senha);
+    
     // Conexão com o banco
     include_once('./conn.php');
+    
     // Instrução SQL
     $sql = "SELECT * FROM moradores WHERE email = '$email' AND senha = '$senha_cripto'";
+    
     // Execução da instrução sql
     $resultadoConsulta = $conn->query($sql);
+    
     // Essa variavel recebe a lista de moradores cadastrados no banco
     $moradores = mysqli_fetch_all($resultadoConsulta);
+    
     // Essas variaveis globais recebem o valor da consulta no banco
     $_SESSION['nome'] = $moradores[0][1];
     $_SESSION['email'] = $moradores[0][2];
