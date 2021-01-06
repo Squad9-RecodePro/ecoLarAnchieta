5c6b1aa4e50b1331cb34a5aac75aa511c96c42f2
FredericoReid <fredericoreid@hotmail.com>
2021-01-05 21:04:21 -0300

    Atualização do formulário de login de usuário

--- frontend/src/pages/RegisterUser/index.jsx
+++ frontend/src/pages/RegisterUser/index.jsx
@@ -6,16 +6,28 @@ import './styles.css';
 const RegisterUser = () => {
     return (
         <div>
-            <form >
-                <div>
-                    <label>registro usuario</label>
-                    <input type="text" name="email" placeholder="Email"/>
+            <form id="form-cadastro" onsubmit="return validateForm(event)" action="http://localhost:8080/ecocasa/backend/cadastro_usuario.php" method="post">
 
+                <div>
+                    <label>Nome</label>
+                    <input type="text" id="nome" name="nome" class="form-control mt-4" placeholder="Nome" required />
                 </div>
+
+                <div>
+                    <label>email</label>
+                    <input type="text" name="email" placeholder="Email"/>
+                </div>
+
                 <div>
                     <label>Senha</label>
-                    <input type="password" name="password" placeholder="Senha"/>
+                    <input type="password" id="senha" name="senha" class="form-control mt-3" placeholder="Senha" required />
                 </div>
+
+                <div>
+                    <label>Confirme a senha: </label>
+                    <input type="password" id="conf_senha" name="conf_senha" class="form-control mt-3" placeholder="Confirmar senha" required />
+                </div>
+
                 <button type="submit">Enviar</button>
             </form>
         </div>
