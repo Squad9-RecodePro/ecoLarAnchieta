82c5123a9a49391d25382d69ab31452e7ecbc2d9
FredericoReid <fredericoreid@hotmail.com>
2021-01-12 18:41:26 -0300

    Ajustado as consultas fetch de acordo com a conexão PDO

--- backend/servidor/acesso_admin.php
+++ backend/servidor/acesso_admin.php
@@ -5,24 +5,27 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 
 if (strlen($email) > 3 && strlen($senha) > 3) {
-    
+    //criptografia da senha
+    $senha_cripto = md5($senha);
+
     // Conexão com o banco
-    //include_once('./conn.php');
-    $conn = mysqli_connect("localhost", "root", "", "ecocasa");
+    include_once('./conn.php');
+    //$conn = mysqli_connect("localhost", "root", "", "ecocasa");
     
     // Instrução SQL
-    $sql = "SELECT * FROM adm WHERE email = '$email' AND senha = '$senha' ";
+    $sql = "SELECT * FROM adm WHERE email = '$email' AND senha = '$senha_cripto' ";
     
     // Execução da instrução sql
     $resultadoConsulta = $conn->query($sql);
     
     // Essa variavel recebe a lista de moradores cadastrados no banco
-    $adm = mysqli_fetch_all($resultadoConsulta);
-    
+    //$adm = mysqli_fetch_all($resultadoConsulta);
+    $adm = $resultadoConsulta->fetch(PDO::FETCH_ASSOC);
+
     // Essas variaveis globais recebem o valor da consulta no banco
-    $_SESSION['nome'] = $adm[0][1];
-    $_SESSION['email'] = $adm[0][2];
-    $_SESSION['senha'] = $adm[0][3];
+    $_SESSION['nome'] = $adm['nome'];
+    $_SESSION['email'] = $adm['email'];
+    $_SESSION['senha'] = $adm['senha'];
     
     // Redirecionado para a pagina de adm
     header('Location: ../../frontend/src/home_admin.php');
