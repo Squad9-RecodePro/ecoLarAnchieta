0c6c1e08b017ca6b069f311aa4412e612e52ac8b
FredericoReid <fredericoreid@hotmail.com>
2021-01-12 14:08:39 -0300

    Consertado a verificação de login com o banco de dados

--- backend/servidor/acesso_login_usuario.php
+++ backend/servidor/acesso_login_usuario.php
@@ -10,19 +10,24 @@ if (strlen($email) > 3 && strlen($senha) > 3) {
     $senha_cripto = md5($senha);
     
     // Conexão com o banco
-    //include_once('./conn.php');
-    $conn = mysqli_connect("localhost", "root", "", "ecocasa");
+    include_once('./conn.php');
+    //$conn = mysqli_connect("localhost", "root", "", "ecocasa");
 
     // Instrução SQL
     $sql = "SELECT * FROM moradores WHERE email = '$email' AND senha = '$senha_cripto'";
+    
     // Execução da instrução sql
     $resultadoConsulta = $conn->query($sql);
+    
     // Essa variavel recebe a lista de moradores cadastrados no banco
-    $moradores = mysqli_fetch_all($resultadoConsulta);
+    $moradores = $resultadoConsulta->fetch(PDO::FETCH_ASSOC);
+    // var_dump($moradores);
+    
     // Essas variaveis globais recebem o valor da consulta no banco
-    $_SESSION['nome'] = $moradores[0][1];
-    $_SESSION['email'] = $moradores[0][2];
-    $_SESSION['senha'] = $moradores[0][3];
+    $_SESSION['nome'] = $moradores['nome'];
+    $_SESSION['email'] = $moradores['email'];
+    $_SESSION['senha'] = $moradores['senha'];
+    
     // Redirecionado para a pagina de user
     header('Location: ../../frontend/src/home_usuario.php');
 }
