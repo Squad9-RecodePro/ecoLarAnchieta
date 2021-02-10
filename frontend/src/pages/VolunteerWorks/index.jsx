4d6bd046ca10829d7c94b12d273f822912efc160
FredericoReid <61941772+FredericoReid@users.noreply.github.com>
2021-02-10 02:02:41 -0300

    Criação da página Trabalhos Voluntários

---
+++ frontend/src/pages/VolunteerWorks/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import MenuVolunteer from '../../components/Menu/Menu VolunteerWorks';
+
+function VolunteerWorks(){
+    return(
+        <>
+            <MenuVolunteer/>
+            <main class="row m-auto container align-items-center py-5" styles="height: 88vh">
+                <div class="table-responsive card">
+                <div class="bg-dark card-header d-flex align-items-center justify-content-between" styles="height: 70px;">
+                    <h4 class="text-white mb-0">Trabalho voluntário</h4>
+                    <button type="button" class="btn btn-outline-light" data-toggle="modal" data-target="#modalExemplo">
+                    Cadastrar
+                    </button>
+                </div>
+                <table class="table table-hover text-center my-0">
+                    <thead>
+                    <tr>
+                        <th scope="col">Título</th>
+                        <th scope="col">Imagem</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col">Número de vagas</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+
+                            <th name="<?php echo $rows['id']; ?>" scope="row"></th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+
+                            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalAlterar">Alterar</button>
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalRemover">Remover</button>
+
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default VolunteerWorks;
