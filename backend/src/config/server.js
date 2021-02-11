ccec87c81d99ae23d234fffab478ed517bbbe901
unknown <thamirez.bastoss@gmail.com>
2021-02-11 11:50:12 -0300

    conectando back com front registerUser

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-
+const cors = require('cors')
 const app = express();
 
+
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// let allowCrossDomain = function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Headers', "*");
+//     next();
+// }
+// app.use(allowCrossDomain);
+
+
 require("../app/controllers/index")(app);
 
-app.listen(3001);
+app.listen(5000);
 
 
 // controllers - endpoints
