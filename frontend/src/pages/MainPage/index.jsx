df5cf9b2e596b7c7b5ee9c08411f98a31fc2db20
Gustavo Cavalcante Ferreira <gustavo2402.00@gmail.com>
2021-02-13 03:44:31 -0300

    section 1 e 2 home

--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -7,12 +7,34 @@ import Menu from '../../components/Menu/mainMenu';
 const MainPage = () => {
     return (
         <>
-        <Menu />
+            <Menu />
 
-        <div className="section-1">
-            <h1>MainPage</h1>
-        </div>
-        
+            <section className="section-1">
+                <hr />
+                <div className="box-1">
+                    <h1><strong>ECO LAR ANCHIETA</strong></h1>
+                    <p className="lead">Projeto desenvolvido pra Ocupação Anchieta, localizada<br />
+                     no Grajaú, ocupação que já é resistência a 7 anos.</p>
+                     <nav>
+                        <button>INSCREVA-SE</button>
+                        <button>LOGIN</button>
+                     </nav>
+                </div>
+            </section>
+            <section className="section-2">
+                <div className="box-2">
+                    <h1>O QUE VOCÊ SABE SOBRE<br /> OCUPAÇÃO?</h1>
+                    <hr />
+                    <p className="lead">The number of applicants placed is down 2% on the <br/> 
+                    same time last year, but overall there has been a <br /> 
+                    steady rise in the numbers going to university over <br /> 
+                    the past 10 years - from 375,320 in 2009.</p>
+                </div>
+                <div className="box-3">
+
+                </div>
+            </section>
+            
         </>
 
     );
