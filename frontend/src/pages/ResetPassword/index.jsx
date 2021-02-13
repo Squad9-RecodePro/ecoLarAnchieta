5736d1208f720db24fd0fdd02cb682ee191d89da
unknown <thamirez.bastoss@gmail.com>
2021-02-13 17:24:35 -0300

    funcionando pagina ForgotPassword e ResetPassword conectada com backend

--- frontend/src/pages/ResetPassword/index.jsx
+++ frontend/src/pages/ResetPassword/index.jsx
@@ -4,18 +4,19 @@ import './styles.css';
 
 const ResetPassword = () => {
     const [email, setEmail] = React.useState("");
+    const [token, setToken] = React.useState("");
     const [password, setPassword] = React.useState("");
     const baseUrl = 'http://localhost:5000/auth/'
 
     let history = useHistory();
 
-    const newPassword = (event) => {
+    const newPassword = async (event) => {
         event.preventDefault();
 
-        const formData = { "email": email, "password": password }
-        const url = `${baseUrl}forgot_password`;
+        const formData = { "email": email, "token": token, "password": password }
+        const url = `${baseUrl}reset_password`;
 
-        fetch(url, {
+        const response = await fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -24,14 +25,14 @@ const ResetPassword = () => {
 
             },
             body: JSON.stringify(formData)
-        }).then(res => res.json())
-            .then((dados) => {
-                if (dados.email) {
-                    history.push("/");
-                } else {
-                    console.log('Não funcionou');
-                }
-            })
+        })
+        let responseStatus = response.status;
+        if (responseStatus === 200) {
+            history.push('UserLogin')
+        } else {
+            console.log(response)
+            alert('Informações inválidas tente novamente');
+        }
 
         event.preventDefault();
     }
@@ -42,24 +43,28 @@ const ResetPassword = () => {
         <>
             <div className="sectionPassword">
                 <div className="content">
-                        <form onSubmit={newPassword}>
-                            <h2>Nova senha</h2>
+                    <form onSubmit={newPassword}>
+                        <h2>Nova senha</h2>
 
-                            <div className="container">
-                                <i className="fa fa-envelope" aria-hidden="true"></i>
-                                <input className="input" type="text" name="email" value={email} onChange={event => setEmail(event.target.value)} placeholder="Email" required />
-                            </div>
-                            <div className="container">
-                                <i className="fa fa-envelope" aria-hidden="true"></i>
-                                <input className="input" type="text" name="password" value={password} onChange={event => setPassword(event.target.value)} placeholder="Senha" required />
-                            </div>
-                            <div className="container">
-                                <i className="fa fa-envelope" aria-hidden="true"></i>
-                                <input className="input" type="text" name="password" placeholder="Confirmar Senha" required />
-                            </div>
-                            <button type="submit" value="Enviar" className="btn container" >Entrar</button>
-                        </form>
-                    </div>
+                        <div className="container">
+                            <i className="fa fa-envelope" aria-hidden="true"></i>
+                            <input className="input" type="text" name="email" value={email} onChange={event => setEmail(event.target.value)} placeholder="Email" required />
+                        </div>
+                        <div className="container">
+                            <i className="fa fa-envelope" aria-hidden="true"></i>
+                            <input className="input" type="text" name="password" value={token} onChange={event => setToken(event.target.value)} placeholder="Token" required />
+                        </div>
+                        <div className="container">
+                            <i className="fa fa-envelope" aria-hidden="true"></i>
+                            <input className="input" type="password" name="password" value={password} onChange={event => setPassword(event.target.value)} placeholder="Nova Senha" required />
+                        </div>
+                        <div className="container">
+                            <i className="fa fa-envelope" aria-hidden="true"></i>
+                            <input className="input" type="password" name="password" placeholder="Confirmar Senha" required />
+                        </div>
+                        <button type="submit" value="Enviar" className="btn container" >Entrar</button>
+                    </form>
+                </div>
             </div>
 
         </>
